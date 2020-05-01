22b7063cfc242b15db019d7e0e944e1b49e19287
Youssef El Ouard <youselouard@gmail.com>
2020-05-01 20:50:43 +0200

    R script to generate first plot and output png file

---
+++ plot1.R
@@ -0,0 +1,18 @@
+## Useful constants to load rows for 2007-02-01 and 2007-02-02 only
+skip <- 66637
+nrows <- 2880
+names <- c("date","time","global_active_power",
+           "global_reactive_power", "voltage", "global_intensity",
+           "sub_metering_1","sub_metering_2","sub_metering_3")
+
+## Load the data (Supposes that the dataset is in the same folder)
+household_data <- read.table("household_power_consumption.txt", sep = ";", skip = skip, nrows = nrows, col.names = names)
+
+## Converts the dates to date format
+household_data$date <- as.Date(household_data$date, format = "%d/%m/%Y")
+
+## Opening the png graphic device, constructing the histogram and closing the device
+png("plot1.png")
+hist(household_data$global_active_power, col="red", main="Global Active Power", 
+     xlab= "Global Active Power (kilowatts)")
+dev.off()
