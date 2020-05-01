a0a49524799687f7ba7c0db94031a6cae9f934f2
Youssef El Ouard <youselouard@gmail.com>
2020-05-01 23:14:13 +0200

    Script and output file for plot4

---
+++ plot4.R
@@ -0,0 +1,27 @@
+## Useful constants to load rows for 2007-02-01 and 2007-02-02 only
+skip <- 66637
+nrows <- 2880
+names <- c("date","time","global_active_power",
+           "global_reactive_power", "voltage", "global_intensity",
+           "sub_metering_1","sub_metering_2","sub_metering_3")
+
+## Load the data (Supposes that the dataset is in the same folder)
+household_data <- read.table("household_power_consumption.txt", sep = ";", skip = skip, nrows = nrows, col.names = names)
+
+## Converts the dates to date format
+household_data$date <- as.Date(household_data$date, format = "%d/%m/%Y")
+
+## Add a column merging time and date
+household_data <- mutate(household_data, full_date = as.POSIXct(paste(date,time)))
+
+## Plot the required graph
+png("plot4.png")
+par(mfrow = c(2,2))
+with(household_data, plot(x= full_date, y=global_active_power, type="l",xlab = "", ylab = "Global active metering"))
+with(household_data, plot(x= full_date, y=voltage, type="l",xlab = "datetime", ylab = "Voltage"))
+with(household_data, plot(x= full_date, y=sub_metering_1, type="l",xlab = "", ylab = "Energy sub metering"))
+with(household_data, lines(x = full_date, y=sub_metering_2, type="l", col="red"))
+with(household_data, lines(x = full_date, y=sub_metering_3, type="l", col="blue"))
+legend("topright", lty = c(1,1), col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+with(household_data, plot(x= full_date, y=global_reactive_power, type="l",xlab = "datetime", ylab = "Global_reactive_power"))
+dev.off()
